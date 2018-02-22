dab91cbd3293371020661f4613bc9a3a1318914c
bramz <brockramz@gmail.com>
2018-02-22 01:27:19 +0000

    Add sqlite3 for persistence

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
+	"database/sql"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+	"net/http"
 )
 
-func randomQuote(context *gin.Context) {
-    context.HTML(http.StatusOK, "public/index.tmpl", gin.H{
-        "title": "RootWire Quote Database",
-    })
-}
+var db *sql.DB
 
 func submitQuote(context *gin.Context) {
+	context.HTML(http.StatusOK, "public/index.tmpl", gin.H{
+		"title": "RootWire Quote Database",
+	})
+
+	quote := context.PostForm("quote")
+
+	stmt, err := db.Prepare("INSERT INTO qdb (quote) values (?)")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+    res, err := stmt.Exec(quote)
+    if err != nil {
+        fmt.Println(err)
+    } else {
+        fmt.Println("query executed")
+    }
+}
+
+func randomQuote(context *gin.Context) {
 }
 
 func searchQuote(context *gin.Context) {
@@ -21,17 +40,22 @@ func deleteQuote(context *gin.Context) {
 }
 
 func main() {
-    router := gin.Default()
+	router := gin.Default()
 
-//    router.LoadGLOB("public/*")
-    router.LoadHTMLFiles("public/index.tmpl")
+	connect, err := sql.Open("sqlite3", "./qdb.db")
 
-    router.GET("/", randomQuote)
-    router.POST("/submit", submitQuote)
-    router.POST("/search/:id", searchQuote)
-    router.POST("/delete/:id", deleteQuote)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    router.Run(":8080")
+    db = connect
+	//    router.LoadGLOB("public/*")
+	router.LoadHTMLFiles("public/index.tmpl")
+
+	router.GET("/", submitQuote)
+	router.POST("/submit", submitQuote)
+	router.POST("/search/:id", searchQuote)
+	router.POST("/delete/:id", deleteQuote)
+	router.GET("/random", randomQuote)
+	router.Run(":8080")
 }
-
-
