bf8a1bc74cd71097a2cae48762880dd8e39ecf5f
bramz <brockramz@gmail.com>
2018-02-24 02:46:15 +0000

    Implemented search by id function

--- main.go
+++ main.go
@@ -30,32 +30,63 @@ func submitQuote(context *gin.Context) {
 }
 
 func randomQuote(context *gin.Context) {
-    rows, err := db.Query("SELECT id, quote FROM quotes ORDER BY RANDOM() LIMIT 1")
-    if err != nil {
-        panic(err)
-    }
+	rows, err := db.Query("SELECT id, quote FROM quotes ORDER BY RANDOM() LIMIT 1")
+	if err != nil {
+		panic(err)
+	}
 
-    var id int
-    var quote string
+	var id int
+	var quote string
 
-    for rows.Next(){
-        err = rows.Scan(&id, &quote)
+	for rows.Next() {
+		err = rows.Scan(&id, &quote)
 
-        if err != nil {
-            panic(err)
-        }
+		if err != nil {
+			panic(err)
+		}
 
-        context.HTML(http.StatusOK, "public/index.tmpl", gin.H{
-            "header": "Random Quotes",
-            "id": id,
-            "quote": quote,
-        })
-    }
+		context.HTML(http.StatusOK, "public/index.tmpl", gin.H{
+			"header": "Random Quotes",
+			"id":     id,
+			"quote":  quote,
+		})
+	}
 
 }
 
 func searchQuote(context *gin.Context) {
+	context.HTML(http.StatusOK, "public/search.tmpl", gin.H{
+		"header": "Search Quotes",
+	})
 
+	id := context.PostForm("id")
+
+	var created string
+	var quote string
+
+    stmt, err := db.Prepare("select created, quote from quotes where id = ?")
+
+//	rows, err := db.Query("select created, quote from quotes where id = ?", 1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer stmt.Close()
+
+    rows, err := stmt.Query(id)
+
+    if err != nil {
+        panic(err)
+    }
+
+	for rows.Next() {
+		err := rows.Scan(&created, &quote)
+		if err != nil {
+			panic(err)
+		}
+		context.String(http.StatusOK, fmt.Sprintf("<p>id '%s'  created '%s' quote '%s'</p>", id, created, quote))
+	}
 }
 
 func deleteQuote(context *gin.Context) {
@@ -63,7 +94,6 @@ func deleteQuote(context *gin.Context) {
 
 func main() {
 	router := gin.Default()
-
 	connect, err := sql.Open("sqlite3", "qdb.db")
 
 	if err != nil {
@@ -73,12 +103,14 @@ func main() {
 	db = connect
 	defer db.Close()
 
-	router.LoadHTMLFiles("public/index.tmpl", "public/submit.tmpl")
+	router.LoadHTMLFiles("public/index.tmpl", "public/submit.tmpl", "public/search.tmpl", "public/output.tmpl")
 
 	router.GET("/", randomQuote)
 	router.GET("/submit", submitQuote)
-    router.POST("/submit", submitQuote)
-	router.POST("/search/:id", searchQuote)
+	router.POST("/submit", submitQuote)
+	router.GET("/search", searchQuote)
+	router.POST("/search", searchQuote)
 	router.POST("/delete/:id", deleteQuote)
+
 	router.Run(":8080")
 }
